ce320118fe9980b0841525970119e17c8b6dda47
h-chenchen <luyb@xunzhaotech.com>
2024-07-19 08:42:34 +0800

    fix -优化首页基础结构

--- _pages/index.blade.php
+++ _pages/index.blade.php
@@ -759,239 +759,110 @@
         }
     </style>
 </head>
-<body class="bg-gradient-to-br from-gray-900 to-black">
-    <div
-      class="text-gray-300 container mx-auto p-8 overflow-hidden md:rounded-lg md:p-10 lg:p-12"
-    >
-      <div class="flex justify-between">
-        <h1 class="font-serif text-3xl font-medium">Landing</h1>
-        <a
-          href="https://github.com/gary149/landing-gradients"
-          class="self-start px-3 py-2 leading-none text-gray-200 border border-gray-800 rounded-lg focus:outline-none focus:shadow-outline bg-gradient-to-b hover:from-indigo-500 from-gray-900 to-black"
-        >
-          Get this template
-        </a>
+<body class="flex flex-col min-h-screen overflow-x-hidden antialiased min-w-screen app-gradient">
+  <main class="bg-gradient-to-b from-slate-900 to-black">
+      <div class="mx-auto max-w-7xl">
+          <!-- Main Hero Content -->
+          <div class="container max-w-lg px-4 py-32 mx-auto text-left md:max-w-none md:text-center">
+              <h1
+                  class="text-5xl font-extrabold leading-10 tracking-tight text-left text-gray-100 md:text-center sm:leading-none md:text-6xl lg:text-7xl">
+                  <span class="block mb-4 text-4xl md:text-5xl sm:mb-0">一个基于微服务架构生态框架</span><span
+                      class="relative mt-2 tracking-normal text-transparent bg-clip-text bg-gradient-to-br logo-gradient md:inline-block drop-shadow-2xl">MicroDesign Framework Ecosystem</span>
+              </h1>
+              <div class="mx-auto mt-8 text-gray-200 sm:mt-4 md:mt-8 md:max-w-2xl md:text-center">
+                  <!-- <section aria-label="About Hyde">
+                      <p class="lg:text-lg">
+                        一个基于微服务架构生态的文档框架
+                      </p>
+                  </section> -->
+
+                  <section aria-label="About this page">
+                      <p class="mt-4 mb-4">
+                          极致体验，用心创造，让开发文档书写更简单、更优雅、更高效!
+
+                          <!-- Syntax highlighted by torchlight.dev -->
+                      <pre style="margin-top: 1.5rem; margin-bottom: 1.5rem;"><code data-theme="material-theme-palenight" data-lang="bash" class="torchlight" style="background-color: #292D3E; padding: 0.5rem 1rem; border-radius: 0.25rem;"><span style="color: #FFCB6B;">npm install</span> <span style="color: #C3E88D;">micro-design-ui</span></code></pre>
+                      </p>
+                  </section>
+
+                  <div class="mt-4 text-white md:mt-8">
+                      <span class="sr-only">快速入口</span>
+                      <ul class="flex flex-wrap justify-center list-none" style="padding: 0;">
+                          <li>
+                              <a href="/micro-design-docsite/docs/index" class="flex m-2 mx-3 text-sm font-bold text-center uppercase">
+                                  文档
+                              </a>
+                          </li>
+                          <li>
+                              <a href="/micro-design-docsite/docs/getting-started" class="flex m-2 mx-3 text-sm font-bold text-center uppercase">
+                                  快速开始
+                              </a>
+                          </li>
+                          <li>
+
+                              <a href="https://github.com/xunzhaotech" class="flex m-2 mx-3 text-sm font-bold text-center">
+                                  GitHub
+                              </a>
+                          </li>
+                          <li>
+                              <a href="https://gitee.com/xunzhaotech" class="flex m-2 mx-3 text-sm font-bold text-center">
+                                  Gitee
+                              </a>
+                          </li>
+                      </ul>
+                  </div>
+              </div>
+          </div>
+          <!-- End Main Hero Content -->
       </div>
-
-      <div class="h-32 md:h-40"></div>
-
+  </main>
+  <div class="grid gap-8 md:grid-cols-3">
+    <div class="flex flex-col justify-center md:col-span-2">
       <p
-        class="font-sans text-4xl font-bold text-gray-200 max-w-5xl lg:text-7xl lg:pr-24 md:text-6xl"
+        class="self-start inline font-sans text-xl font-medium text-transparent bg-clip-text bg-gradient-to-br from-teal-400 to-teal-600"
       >
-        Spend less time coding and more time creating
+        We are humans
       </p>
-      <div class="h-10"></div>
-      <p class="max-w-2xl font-serif text-xl text-gray-400 md:text-2xl">
-        Imagine being able to spent less time... This is a demonstration landing
-        page made with tailwindcss
+      <h2 class="text-4xl font-bold">We could work together</h2>
+      <div class="h-6"></div>
+      <p class="font-serif text-xl text-gray-400 md:pr-10">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam
+        autem, a recusandae vero praesentium qui impedit doloremque
+        molestias.
       </p>
-
-      <div class="h-32 md:h-40"></div>
-
-      <div class="grid gap-8 md:grid-cols-2">
-        <div class="flex flex-col justify-center">
-          <p
-            class="self-start inline font-sans text-xl font-medium text-transparent bg-clip-text bg-gradient-to-br from-green-400 to-green-600"
-          >
-            Simple and easy
+      <div class="h-8"></div>
+      <div class="grid gap-6 pt-8 border-t border-gray-800 lg:grid-cols-3">
+        <div>
+          <p class="font-semibold text-gray-400">Made with love</p>
+          <div class="h-4"></div>
+          <p class="font-serif text-gray-400">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Delectus labor.
           </p>
-          <h2 class="text-4xl font-bold">Made for devs and designers</h2>
-          <div class="h-6"></div>
-          <p class="font-serif text-xl text-gray-400 md:pr-10">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam
-            autem, a recusandae vero praesentium qui impedit doloremque
-            molestias necessitatibus.
-          </p>
-          <div class="h-8"></div>
-          <div class="grid grid-cols-2 gap-4 pt-8 border-t border-gray-800">
-            <div>
-              <p class="font-semibold text-gray-400">Made with love</p>
-              <div class="h-4"></div>
-              <p class="font-serif text-gray-400">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Delectus labor.
-              </p>
-            </div>
-            <div>
-              <p class="font-semibold text-gray-400">It's easy to build</p>
-              <div class="h-4"></div>
-              <p class="font-serif text-gray-400">
-                Ipsum dolor sit, amet consectetur adipisicing elit. Delectus
-                amet consectetur.
-              </p>
-            </div>
-          </div>
         </div>
         <div>
-          <div
-            class="-mr-24 rounded-lg md:rounded-l-full bg-gradient-to-br from-gray-900 to-black h-96"
-          ></div>
-        </div>
-      </div>
-
-      <div class="h-32 md:h-40"></div>
-
-      <p class="font-serif text-4xl">
-        <span class="text-gray-400">If we work all together</span>
-
-        <span class="text-gray-600"
-          >consectetur adipisicing elit. Consectetur atque molestiae omnis
-          excepturi enim!</span
-        >
-      </p>
-
-      <div class="h-32 md:h-40"></div>
-
-      <div class="grid gap-4 md:grid-cols-3">
-        <div
-          class="flex-col p-8 py-16 rounded-lg shadow-2xl md:p-12 bg-gradient-to-br from-gray-900 to-black"
-        >
-          <p
-            class="flex items-center justify-center text-4xl font-semibold text-green-400 bg-green-800 rounded-full shadow-lg w-14 h-14"
-          >
-            1
+          <p class="font-semibold text-gray-400">It's easy to build</p>
+          <div class="h-4"></div>
+          <p class="font-serif text-gray-400">
+            Ipsum dolor sit, amet consectetur adipisicing elit. Delectus
+            amet consectetur.
           </p>
-          <div class="h-6"></div>
-          <p class="font-serif text-3xl">We build products with UX in mind</p>
-        </div>
-        <div
-          class="flex-col p-8 py-16 rounded-lg shadow-2xl md:p-12 bg-gradient-to-b from-gray-900 to-black"
-        >
-          <p
-            class="flex items-center justify-center text-4xl font-semibold text-indigo-400 bg-indigo-800 rounded-full shadow-lg w-14 h-14"
-          >
-            2
-          </p>
-          <div class="h-6"></div>
-          <p class="font-serif text-3xl">
-            You can trust us to deliver super fast
-          </p>
-        </div>
-        <div
-          class="flex-col p-8 py-16 rounded-lg shadow-2xl md:p-12 bg-gradient-to-bl from-gray-900 to-black"
-        >
-          <p
-            class="flex items-center justify-center text-4xl font-semibold text-teal-400 bg-teal-800 rounded-full shadow-lg w-14 h-14"
-          >
-            3
-          </p>
-          <div class="h-6"></div>
-          <p class="font-serif text-3xl">We made it simple and easy to do</p>
-        </div>
-      </div>
-
-      <div class="h-40"></div>
-
-      <div class="grid gap-8 md:grid-cols-3">
-        <div class="flex flex-col justify-center md:col-span-2">
-          <p
-            class="self-start inline font-sans text-xl font-medium text-transparent bg-clip-text bg-gradient-to-br from-teal-400 to-teal-600"
-          >
-            We are humans
-          </p>
-          <h2 class="text-4xl font-bold">We could work together</h2>
-          <div class="h-6"></div>
-          <p class="font-serif text-xl text-gray-400 md:pr-10">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam
-            autem, a recusandae vero praesentium qui impedit doloremque
-            molestias.
-          </p>
-          <div class="h-8"></div>
-          <div class="grid gap-6 pt-8 border-t border-gray-800 lg:grid-cols-3">
-            <div>
-              <p class="font-semibold text-gray-400">Made with love</p>
-              <div class="h-4"></div>
-              <p class="font-serif text-gray-400">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Delectus labor.
-              </p>
-            </div>
-            <div>
-              <p class="font-semibold text-gray-400">It's easy to build</p>
-              <div class="h-4"></div>
-              <p class="font-serif text-gray-400">
-                Ipsum dolor sit, amet consectetur adipisicing elit. Delectus
-                amet consectetur.
-              </p>
-            </div>
-            <div>
-              <p class="font-semibold text-gray-400">It's easy to build</p>
-              <div class="h-4"></div>
-              <p class="font-serif text-gray-400">
-                Ipsum dolor sit, amet consectetur adipisicing elit. Delectus
-                amet consectetur.
-              </p>
-            </div>
-          </div>
         </div>
         <div>
-          <div
-            class="-mr-24 rounded-lg md:rounded-l-full bg-gradient-to-br from-gray-900 to-black h-96"
-          ></div>
+          <p class="font-semibold text-gray-400">It's easy to build</p>
+          <div class="h-4"></div>
+          <p class="font-serif text-gray-400">
+            Ipsum dolor sit, amet consectetur adipisicing elit. Delectus
+            amet consectetur.
+          </p>
         </div>
       </div>
-
-      <div class="h-10 md:h-40"></div>
-
-      <div class="grid gap-4 md:grid-cols-4">
-        <ul class="space-y-1 text-gray-400">
-          <li class="pb-4 font-serif text-gray-400">Social networks</li>
-          <li>
-            <a href="https://twitter.com/victormustar" class="hover:underline"
-              >Twitter</a
-            >
-          </li>
-          <li>
-            <a href="#" class="hover:underline">Linkedin</a>
-          </li>
-          <li>
-            <a href="#" class="hover:underline">Facebook</a>
-          </li>
-        </ul>
-        <ul class="space-y-1 text-gray-400">
-          <li class="pb-4 font-serif text-gray-400">Locations</li>
-          <li>
-            <a href="#" class="hover:underline">Paris</a>
-          </li>
-          <li>
-            <a href="#" class="hover:underline">New York</a>
-          </li>
-          <li>
-            <a href="#" class="hover:underline">London</a>
-          </li>
-          <li>
-            <a href="#" class="hover:underline">Singapour</a>
-          </li>
-        </ul>
-        <ul class="space-y-1 text-gray-400">
-          <li class="pb-4 font-serif text-gray-400">Company</li>
-          <li>
-            <a href="#" class="hover:underline">The team</a>
-          </li>
-          <li>
-            <a href="#" class="hover:underline">About us</a>
-          </li>
-          <li>
-            <a href="#" class="hover:underline">Our vision</a>
-          </li>
-          <li>
-            <a href="#" class="hover:underline">Join us</a>
-          </li>
-        </ul>
-        <ul class="space-y-1 text-gray-400">
-          <li class="pb-4 font-serif text-gray-400">Join</li>
-          <li>
-            <a
-              href="https://github.com/gary149/landing-gradients"
-              class="self-start px-3 py-2 leading-none text-gray-200 border border-gray-800 rounded-lg focus:outline-none focus:shadow-outline bg-gradient-to-b hover:from-indigo-500 from-gray-900 to-black"
-            >
-              Get this template
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div class="h-12"></div>
     </div>
+    <div>
+      <div
+        class="-mr-24 rounded-lg md:rounded-l-full bg-gradient-to-br from-gray-900 to-black h-96"
+      ></div>
+    </div>
+  </div>
   </body>
-</html>
 </html>
