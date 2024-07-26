db1814a865d113b82912f2812ceee5cfbf44bcb2
h-chenchen <luyb@xunzhaotech.com>
2024-07-26 10:26:16 +0800

    fix -优化首页界面

--- _pages/index.blade.php
+++ _pages/index.blade.php
@@ -816,53 +816,5 @@
           <!-- End Main Hero Content -->
       </div>
   </main>
-  <div class="grid gap-8 md:grid-cols-3">
-    <div class="flex flex-col justify-center md:col-span-2">
-      <p
-        class="self-start inline font-sans text-xl font-medium text-transparent bg-clip-text bg-gradient-to-br from-teal-400 to-teal-600"
-      >
-        We are humans
-      </p>
-      <h2 class="text-4xl font-bold">We could work together</h2>
-      <div class="h-6"></div>
-      <p class="font-serif text-xl text-gray-400 md:pr-10">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam
-        autem, a recusandae vero praesentium qui impedit doloremque
-        molestias.
-      </p>
-      <div class="h-8"></div>
-      <div class="grid gap-6 pt-8 border-t border-gray-800 lg:grid-cols-3">
-        <div>
-          <p class="font-semibold text-gray-400">Made with love</p>
-          <div class="h-4"></div>
-          <p class="font-serif text-gray-400">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Delectus labor.
-          </p>
-        </div>
-        <div>
-          <p class="font-semibold text-gray-400">It's easy to build</p>
-          <div class="h-4"></div>
-          <p class="font-serif text-gray-400">
-            Ipsum dolor sit, amet consectetur adipisicing elit. Delectus
-            amet consectetur.
-          </p>
-        </div>
-        <div>
-          <p class="font-semibold text-gray-400">It's easy to build</p>
-          <div class="h-4"></div>
-          <p class="font-serif text-gray-400">
-            Ipsum dolor sit, amet consectetur adipisicing elit. Delectus
-            amet consectetur.
-          </p>
-        </div>
-      </div>
-    </div>
-    <div>
-      <div
-        class="-mr-24 rounded-lg md:rounded-l-full bg-gradient-to-br from-gray-900 to-black h-96"
-      ></div>
-    </div>
-  </div>
 </body>
 </html>
