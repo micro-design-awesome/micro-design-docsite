87e0c6ac69c8cdc246826f5a7eb06ddf2fa881bc
h-chenchen <luyb@xunzhaotech.com>
2024-07-26 12:02:18 +0800

    fix -修改首页文章

--- _pages/index.blade.php
+++ _pages/index.blade.php
@@ -759,60 +759,62 @@
         }
     </style>
 </head>
-<body>
-    <section class="absolute w-full min-h-screen px-20 py-10 flex justify-between items-center md:flex-row flex-col">
-        <!-- Logo -->
-        <header class="absolute left-0 top-0 w-full py-10 px-24 flex item-center justify-center z-50 text-white">
-            <a href="#" class="flex items-center justify-center">
-                <span class="ml-3 text-3xl font-light text-blue-white uppercase tracking-wider">Data <span class="text-2xl font-semibold">World</span></span>
-            </a>
+<body class="flex flex-col min-h-screen overflow-x-hidden antialiased min-w-screen app-gradient">
+  <main class="bg-gradient-to-b from-slate-900 to-black">
+      <div class="mx-auto max-w-7xl">
+          <!-- Main Hero Content -->
+          <div class="container max-w-lg px-4 py-32 mx-auto text-left md:max-w-none md:text-center">
+              <h1
+                  class="text-5xl font-extrabold leading-10 tracking-tight text-left text-gray-100 md:text-center sm:leading-none md:text-6xl lg:text-7xl">
+                  <span class="block mb-4 text-4xl md:text-5xl sm:mb-0">一个基于微服务架构生态框架</span><span
+                      class="relative mt-2 tracking-normal text-transparent bg-clip-text bg-gradient-to-br logo-gradient md:inline-block drop-shadow-2xl">MicroDesign Framework Ecosystem</span>
+              </h1>
+              <div class="mx-auto mt-8 text-gray-200 sm:mt-4 md:mt-8 md:max-w-2xl md:text-center">
+                  <!-- <section aria-label="About Hyde">
+                      <p class="lg:text-lg">
+                        一个基于微服务架构生态的文档框架
+                      </p>
+                  </section> -->
 
-        </header>
+                  <section aria-label="About this page">
+                      <p class="mt-4 mb-4">
+                          极致体验，用心创造，让开发文档书写更简单、更优雅、更高效!
 
-        <!-- video y overlay -->
-        <video autoplay loop muted class="absolute top-0 left-0 w-full h-full object-cover -z-10">
-            <source src="files/video.mp4" type="video/mp4">
-        </video>
+                          <!-- Syntax highlighted by torchlight.dev -->
+                      <pre style="margin-top: 1.5rem; margin-bottom: 1.5rem;"><code data-theme="material-theme-palenight" data-lang="bash" class="torchlight" style="background-color: #292D3E; padding: 0.5rem 1rem; border-radius: 0.25rem;"><span style="color: #FFCB6B;">npm install</span> <span style="color: #C3E88D;">micro-design-ui</span></code></pre>
+                      </p>
+                  </section>
 
-        <div class="absolute top-0 left-0 w-full h-full bg-black/70"></div>
+                  <div class="mt-4 text-white md:mt-8">
+                      <span class="sr-only">快速入口</span>
+                      <ul class="flex flex-wrap justify-center list-none" style="padding: 0;">
+                          <li>
+                              <a href="/micro-design-docsite/docs/index" class="flex m-2 mx-3 text-sm font-bold text-center uppercase">
+                                  文档
+                              </a>
+                          </li>
+                          <li>
+                              <a href="/micro-design-docsite/docs/getting-started" class="flex m-2 mx-3 text-sm font-bold text-center uppercase">
+                                  快速开始
+                              </a>
+                          </li>
+                          <li>
 
-        <!-- text -->
-        <div class="px-10 py-24 flex flex-wrap flex-col items-start gap-4 z-50 lg:w-3/5 md:1/2">
-            <h1 class="text-5xl text-white font-light"><span class="font-semibold text-6xl uppercase">Nuestros datos</span> Lorem ipsum dolor sit.</h1>
-            <p class="leading-relaxed mt-4 text-gray-200 md:block hidden">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A alias cum, illo in inventore odit repellat totam! Dicta molestias, numquam.</p>
-            <button class="text-black bg-white border-0 py-2 px-8 mt-4 text-md font-semibold tracking-widest uppercase hover:bg-gray-300 transition hover:scale-125">Explora</button>
-        </div>
-
-        <!--iconos -->
-        <div class="z-50 flex md:flex-col sm:flex-row gap-10">
-            <a href="">
-                <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" class="w-6 h-6 text-white/50 hover:text-white hover:scale-150 transition" viewBox="0 0 16 16">
-                    <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
-                </svg>
-            </a>
-            <a href="">
-                <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" class="w-6 h-6 text-white/50 hover:text-white hover:scale-150 transition" viewBox="0 0 16 16">
-                    <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/>
-                </svg>
-            </a>
-            <a href="">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-6 h-6 text-white/50 hover:text-white hover:scale-150 transition" viewBox="0 0 16 16">
-                    <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
-                </svg>
-            </a>
-            <a href="">
-                <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" class="w-6 h-6 text-white/50 hover:text-white hover:scale-150 transition" viewBox="0 0 16 16">
-                    <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.01 2.01 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.01 2.01 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31 31 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.01 2.01 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A100 100 0 0 1 7.858 2zM6.4 5.209v4.818l4.157-2.408z"/>
-                </svg>
-            </a>
-            <a href="">
-                <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" class="w-6 h-6 text-white/50 hover:text-white hover:scale-150 transition" viewBox="0 0 16 16">
-                    <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z"/>
-                </svg>
-            </a>
-
-        </div>
-    </section>
-
+                              <a href="https://github.com/xunzhaotech" class="flex m-2 mx-3 text-sm font-bold text-center">
+                                  GitHub
+                              </a>
+                          </li>
+                          <li>
+                              <a href="https://gitee.com/xunzhaotech" class="flex m-2 mx-3 text-sm font-bold text-center">
+                                  Gitee
+                              </a>
+                          </li>
+                      </ul>
+                  </div>
+              </div>
+          </div>
+          <!-- End Main Hero Content -->
+      </div>
+  </main>
 </body>
 </html>
