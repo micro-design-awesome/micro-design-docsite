800143b6791a5351da99d17d9fb15fae0f5af4eb
h-chenchen <luyb@xunzhaotech.com>
2025-07-12 12:45:11 +0800

    fix -项目集成@documate/documate

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -4,11 +4,14 @@
  * @Email: luyb@xunzhaotech.com
  * @QQ: 1525572900
  * @Date: 2024-08-11 11:40:49
- * @LastEditTime: 2024-10-28 15:57:02
+ * @LastEditTime: 2025-07-12 12:34:15
  * @LastEditors: xunzhaotech
  */
+import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import 'vitepress-theme-demoblock/dist/theme/styles/index.css'
+import Documate from '@documate/vue'
+import '@documate/vue/dist/style.css'
 import { useComponents } from './useComponents'
 import './style.css'
 import './styles/custom.css'
@@ -18,6 +21,11 @@ import '../../../src/styles/index.css'
 
 export default {
   ...DefaultTheme,
+  Layout: h(DefaultTheme.Layout, null, {
+    'nav-bar-content-before': () => h(Documate, {
+      endpoint: '',
+    }),
+  }),
   enhanceApp(ctx) {
     DefaultTheme.enhanceApp(ctx)
     useComponents(ctx.app)
