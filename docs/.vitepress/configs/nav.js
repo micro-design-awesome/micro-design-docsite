a5f359f0be6f6f80f351f524cfb69a5e88902f81
h-chenchen <luyb@xunzhaotech.com>
2025-07-31 16:56:54 +0800

    fix -修改页面基础样式

--- docs/.vitepress/configs/nav.js
+++ docs/.vitepress/configs/nav.js
@@ -4,18 +4,29 @@
  * @Email: luyb@xunzhaotech.com
  * @QQ: 1525572900
  * @Date: 2024-08-11 11:40:49
- * @LastEditTime: 2025-07-12 17:28:29
+ * @LastEditTime: 2025-07-31 16:52:43
  * @LastEditors: xunzhaotech
  */
 import { createRequire } from 'module'
+import { text } from 'stream/consumers'
 const require = createRequire(import.meta.url)
 const pkg = require('../../../package.json')
 export default [
   { text: '指南', link: '/guide/', activeMatch: '^/guide/' },
   { text: '配置', link: '/config/', activeMatch: '^/config/' },
-  { text: '文档', link: '/docs/', activeMatch: '^/docs/' },
-  { text: '组件', link: '/components/', activeMatch: '^/components/' },
-  { text: '项目', link: '/project/', activeMatch: '^/project/' },
+  {
+    text: '项目文档',
+    items: [
+      { text: '项目', link: '/project/', activeMatch: '^/project/' },
+      { text: '文档', link: '/docs/', activeMatch: '^/docs/' },
+  ]},
+  {
+    text: '插件市场',
+    items: [
+      { text: '桌面组件', link: '/components/', activeMatch: '^/components/' },
+      { text: '移动组件', link: '/marketplace/plugins/', activeMatch: '^/marketplace/plugins/' },
+    ]
+  },
   { text: '规范', link: '/lint/', activeMatch: '^/lint/' },
   { text: '课程', link: '/course/', activeMatch: '^/course/' },
   { text: '案例', link: '/store/', activeMatch: '^/store/' },
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -4,7 +4,7 @@
  * @Email: luyb@xunzhaotech.com
  * @QQ: 1525572900
  * @Date: 2024-08-11 11:40:49
- * @LastEditTime: 2025-07-31 12:53:28
+ * @LastEditTime: 2025-07-31 16:54:21
  * @LastEditors: xunzhaotech
  */
 import { h } from 'vue'
@@ -34,8 +34,11 @@ export default {
     useComponents(ctx.app)
     ctx.app.component(Button.name, Button)
     ctx.app.component('HomeHero', () => import('./components/HomeHero.vue'))
-    ctx.app.component('FeatureCard', () => import('./components/FeatureCard.vue'))
     ctx.app.component('CodePreview', () => import('./components/CodePreview.vue'))
+    ctx.app.component('ProductHero', () => import('./components/ProductHero.vue'))
+    ctx.app.component('FeatureCard', () => import('./components/FeatureCard.vue'))
+    ctx.app.component('PricingCard', () => import('./components/PricingCard.vue'))
+    ctx.app.component('AppFooter', () => import('./components/Footer.vue'))
     ctx.app.provide('collapsedIcon', {
         open: '▼', // 展开时的图标
         closed: '▶' // 折叠时的图标
