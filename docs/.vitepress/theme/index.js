da7dec6776996dbd2984b9f6fcb9c8c3c0dda272
h-chenchen <luyb@xunzhaotech.com>
2025-08-02 10:11:59 +0800

    fix -集成动画功能

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -4,11 +4,13 @@
  * @Email: luyb@xunzhaotech.com
  * @QQ: 1525572900
  * @Date: 2024-08-11 11:40:49
- * @LastEditTime: 2025-08-01 19:14:00
+ * @LastEditTime: 2025-08-02 10:08:24
  * @LastEditors: xunzhaotech
  */
 import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
+// import { InspiraUI } from 'inspira-ui';
+// import 'inspira-ui/dist/style.css'; // 引入样式
 import 'uno.css'
 import 'vitepress-theme-demoblock/dist/theme/styles/index.css'
 import Documate from '@documate/vue'
@@ -35,6 +37,7 @@ export default {
   enhanceApp(ctx) {
     DefaultTheme.enhanceApp(ctx)
     useComponents(ctx.app)
+    // ctx.app.use(InspiraUI)
     ctx.app.component(Button.name, Button)
     ctx.app.component('HomeHero', () => import('./components/HomeHero.vue'))
     ctx.app.component('CodePreview', () => import('./components/CodePreview.vue'))
